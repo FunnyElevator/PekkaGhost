ad298651428034798cba147a6b1ace95e09b78e6
Fabian Häusler <fabian.haeusler1@gmail.com>
2014-10-23 13:58:31 +0300

    Fixed start screen blur effect

--- PekkaGhost/ViewController.m
+++ PekkaGhost/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *blurView;
 @end
 
 @implementation ViewController
@@ -17,6 +18,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+}
+- (void)viewDidAppear:(BOOL)animated {
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    [blurEffectView setFrame:self.blurView.bounds];
+    [self.blurView addSubview:blurEffectView];
+    [self.blurView setBackgroundColor:[UIColor colorWithWhite:1.0 alpha:0.4]];
 }
 
 - (void)didReceiveMemoryWarning {
